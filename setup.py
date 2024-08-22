b4570089e0bafe40fc7c0d5c18a5645826c0a99c
Wendel Schaatsbergen <wendel.schaatsbergen@springernature.com>
2024-08-22 14:46:58 +0200

    Added python versions 3.10, 3.11 and 3.12 to the github tests

--- setup.py
+++ setup.py
@@ -41,6 +41,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
 
         # License
         'License :: OSI Approved :: MIT License',
