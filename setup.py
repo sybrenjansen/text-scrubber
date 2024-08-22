6631a54995cc85f5f2b721f5394f08380da3e14e
Wendel Schaatsbergen <wendel.schaatsbergen@springernature.com>
2024-08-22 16:54:44 +0200

    Revert "Upgraded sphinx-versions to 1.1.3"
    This reverts commit 724f95d9

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     extras_require={'docs': ['sphinx>=5,<6',
                              'sphinx-rtd-theme==0.5.0',
                              'sphinx-autodoc-typehints==1.11.0',
-                             'sphinx-versions==1.1.3',
+                             'sphinx-versions==1.0.1',
                              'click==8.0.4'],
                     'tests': ['nose2', 'numpy']},
     test_suite='nose2.collector.collector',
